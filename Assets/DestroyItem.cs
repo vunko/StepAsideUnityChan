76e03375812fc658bba0cc013865052e81446e32
奈良　智樹 <niconicosenyouv@gmail.com>
2017-11-09 14:42:23 +0900

    あああ

---
+++ Assets/DestroyItem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyItem : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject Camera = GameObject.Find("Main Camera");
+		if (this.transform.position.z < Camera.transform.position.z) {
+			Destroy (this.gameObject);
+		}
+		
+	}
+}
